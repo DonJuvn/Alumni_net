e10a67e5e5901d6ba295084b0f915f911ba584bd
rubyazz <yersultan.abduvalov@beksar.kz>
2024-02-02 14:58:17 +0600

    setting up navigation and home page

--- client/src/components/navigation.jsx
+++ client/src/components/navigation.jsx
@@ -1,14 +1,26 @@
-// import React from "react";import { Route, Routes } from "react-router-dom";
-// export const Navigation = () => {
-//    return (
-//       <div>
-//          <ul>
-//             <li></li>
-//             <li></li>
-//             <li></li>
-//             <li></li>
-//             <li></li>
-//          </ul>
-//       </div>
-//    );
-// };
+// components/navigation.jsx
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/news">News</Link>
+        </li>
+        <li>
+          <Link to="/products">Products</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
